933cdf0d16a98a0d89c282d70da31df42ed8f42e
Izzy Oji <eoji11592@gmail.com>
2018-04-26 21:36:03 -0400

    views (from the 6)

--- app/src/main/java/com/example/lisaluo/kotlinproject/MainActivity.java
+++ app/src/main/java/com/example/lisaluo/kotlinproject/MainActivity.java
@@ -4,13 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public final class MainActivity extends Activity {
+
+    @BindView(R.id.emoji) ImageView emoji;
+    @BindView(R.id.speaker_name) TextView name;
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     public void configureWith(@NonNull final Person person) {
@@ -21,14 +31,17 @@ public final class MainActivity extends Activity {
         // start the background color animation
     }
 
+    @OnClick(R.id.lisa)
     public void dancerClicked() {
         configureWith(Person.LISA);
     }
 
+    @OnClick(R.id.lisa)
     public void sleuthOrSpyClicked() {
         configureWith(Person.IZZY);
     }
 
+    @OnClick(R.id.kevin)
     public void thinkingFaceClicked() {
         configureWith(Person.KEVIN);
     }
