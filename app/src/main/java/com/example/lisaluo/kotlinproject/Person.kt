6eaf490be6d4f06610c2b69ffccf0e209080a1b9
Izzy Oji <eoji11592@gmail.com>
2018-05-01 21:21:05 -0400

    updating the lookz

--- app/src/main/java/com/example/lisaluo/kotlinproject/Person.kt
+++ app/src/main/java/com/example/lisaluo/kotlinproject/Person.kt
@@ -1,7 +1,7 @@
 package com.example.lisaluo.kotlinproject
 
 enum class Person(val firstName: String, val favoriteColor: Int, val emojiImageId: Int) {
-    IZZY("Izzy", R.color.blue, R.mipmap.sleuth_or_spy),
-    LISA("Lisa", R.color.chartreuse, R.mipmap.dancer),
-    KEVIN("Kevin", R.color.red, R.mipmap.thinking_face)
+    IZZY("Izzy", R.color.blue, R.drawable.izzy),
+    LISA("Lisa", R.color.chartreuse, R.drawable.lisa),
+    KEVIN("Kevin", R.color.red, R.drawable.kevin)
 }
